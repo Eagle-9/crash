5eb686769bfd07d1e202e21d2b1b7c6bbb98245f
Mason Coles <masocoles@gmail.com>
2024-02-14 16:36:55 -0500

    added "script mode" execution

--- src/main.cc
+++ src/main.cc
@@ -1,9 +1,47 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
 // include implementation
 #include <impl.hh>
 
-int main()
+int main(int argc, char** argv)
 {
+    if (argc == 2)
+    {
+        // open the file
+        std::ifstream content;
+        content.open(argv[1]);
+
+        // make sure the file opened properly
+        if (!content.is_open())
+        {
+            std::cerr << "Failed to open file: " << argv[1] << std::endl;
+            return 1;
+        }
+
+        // to store all the lines
+        std::vector<std::string> lines;
+        // to store each line temporarily
+        std::string line;
+        // read all lines to 'lines'
+        while (getline(content, line))
+        {
+          lines.push_back(line);
+        }
+
+        // close the file;
+        // all lines are now in memory, in 'lines'
+        content.close();
+
+        for (size_t i = 0; i < lines.size(); i++)
+        {
+            // todo: print what we're doing (echo) (do we need this?)
+            parse(lines[i]);
+        }
+
+        return 0;
+    }
+
     // to store the line
     std::string line;
     // initial prompt
