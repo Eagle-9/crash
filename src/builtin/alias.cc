23b101c6f7b6ac6e79d6ebe055be2abec18f2cdf
sheepman39 <jsalata90@gmail.com>
2024-03-13 18:40:23 -0400

    TODO: add ability to add aliases

--- src/builtin/alias.cc
+++ src/builtin/alias.cc
@@ -10,9 +10,13 @@ void alias_print(void)
 
 int builtin_alias(int argc, char **argv)
 {
+    // print out each alias if we just run 'alias' or with -p
     if(argc == 1 || (argc >= 2 && strcmp(argv[1],"-p") == 0))
     {
         alias_print();
     } 
+    else {
+        
+    }
     return 0;
 }
