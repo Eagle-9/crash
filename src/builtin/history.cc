3d90d1f776f3ff61f9692515b929466719c2b401
sheepman39 <jsalata90@gmail.com>
2024-03-13 12:36:43 -0400

    Fixed the history help command

--- src/builtin/history.cc
+++ src/builtin/history.cc
@@ -5,10 +5,10 @@ int history_help_message(int argc, char ** argv)
 {
 
     // simple help message
-    std::string simpleHelp = "";
+    std::string simpleHelp = "Simply type 'history' to view the history of your files. Use -H for full help message";
 
     // full help message
-    std::string fullHelp = "";
+    std::string fullHelp = "CRASH MANUAL -- HOW TO USE 'history'\n\nhistory [-h] [-H] [-l [{n}]] [-{n}] [-c] [-s] \n\nGeneral Use\n\nPrint out the full history of your commands\n\nArguments\n\n-h : Display simple help message\n-H : Display full help message\n-l [{n}] : Display the history list of the commands with serial numbers. With the optional N it displays only the last N entries\n-{n} : pnly print out the n'th command\n-c : Clean the directory history\n-s : Suppress the directory history by eliminating duplicated directories. The order of the history must be maintained. The latest entry must be kept for same directories";
 
     // differentiate between simple and complex help message
     if (strcmp(argv[1], "-h") == 0)
